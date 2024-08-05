b4b50507a739ee2013d51c82982615fd6aeede5c
rogermparent <rogermparent@gmail.com>
2024-08-04 21:10:07 -0400

    Update eslint configs for 9+

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,4 @@
 const config = {
-  extends: ["next/core-web-vitals", "plugin:cypress/recommended"],
-  plugins: ["cypress", "mocha"],
-  rules: {
-    "mocha/no-skipped-tests": "error",
-    "mocha/no-exclusive-tests": "error",
-  },
   ignores: [
     "/node_modules",
     "/.pnp",
--- websites/recipe-website/editor/eslint.config.mjs
+++ websites/recipe-website/editor/eslint.config.mjs
@@ -1,10 +1,21 @@
-const config = {
-  extends: ["next/core-web-vitals", "plugin:cypress/recommended"],
-  plugins: ["cypress", "mocha"],
-  rules: {
-    "mocha/no-skipped-tests": "error",
-    "mocha/no-exclusive-tests": "error",
-  },
-};
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
 
-export default config;
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": hooksPlugin,
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...hooksPlugin.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      "@next/next/no-img-element": "error",
+    },
+  },
+];
