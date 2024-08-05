8f5ee9bccfc02f4edc0f6b18396a6367b6334eed
Roger <rogermparent@gmail.com>
2024-08-04 21:27:52 -0400

    Add GitHub Actions workflow for lint (#51)
    * Add GH workflow for lint-staged
    
    * change name on new action
    
    * Change second checkout to run git fetch instead
    
    * change diff command
    
    * change command again
    
    * Use --check over --write in lint-staged
    
    * Add permissions to lint action
    
    * add test diff
    
    * add test flags
    
    * use pnpm exec
    
    * use pnpm in action
    
    * Remove test flags
    
    * Remove unnecessary quotes and update trunk
    
    * Use lint-staged.config.js and run tests when js/ts changes
    
    * Use external eslint config
    
    * use new eslint format
    
    * use mjs for eslint configs
    
    * Update eslint configs for 9+
    
    * Upgrade eslint
    
    * Remove test from lint-staged

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+const config = {
+  ignores: [
+    "/node_modules",
+    "/.pnp",
+    ".pnp.js",
+    ".yarn/install-state.gz",
+    "/website/.next/",
+    "/editor/.next/",
+
+    "/website/out/",
+
+    "/content",
+    "/test-content",
+  ],
+};
+
+export default config;
