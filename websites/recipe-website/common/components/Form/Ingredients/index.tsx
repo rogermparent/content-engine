fb97498a2fb874ea92779ae5f227bacdd7f32149
rogermparent <rogermparent@gmail.com>
2024-08-06 17:20:01 -0400

    combine ingredient input list and single

--- websites/recipe-website/common/components/Form/Ingredients/index.tsx
+++ websites/recipe-website/common/components/Form/Ingredients/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { RecipeFormErrors } from "../../../controller/formState";
 import { Ingredient } from "../../../controller/types";
 import { createIngredients } from "../../../util/parseIngredients";
@@ -10,9 +11,30 @@ import {
   InputListControls,
   useKeyList,
 } from "component-library/components/Form/inputs/List";
-import IngredientInput from "./IngredientInput";
-import clsx from "clsx";
 import { useRef } from "react";
+import { InlineMarkdownInput } from "component-library/components/Form/inputs/Markdown/Inline";
+import { MarkdownInputProps } from "component-library/components/Form/inputs/Markdown/common";
+import { DummyMultiplyable, RecipeCustomControls } from "../RecipeMarkdown";
+
+export default function IngredientInput({
+  name,
+  id,
+  label,
+  defaultValue,
+  errors,
+}: MarkdownInputProps) {
+  return (
+    <InlineMarkdownInput
+      name={name}
+      id={id}
+      label={label}
+      defaultValue={defaultValue}
+      errors={errors}
+      Controls={RecipeCustomControls}
+      components={{ Multiplyable: DummyMultiplyable }}
+    />
+  );
+}
 
 export function IngredientsListInput({
   name,
