2e2d7a1293e4aca6ebf820b064e0433f50fb2634
rogermparent <rogermparent@gmail.com>
2024-09-25 15:13:35 -0400

    tests b

--- websites/recipe-website/editor/cypress/e2e/git.cy.ts
+++ websites/recipe-website/editor/cypress/e2e/git.cy.ts
@@ -1,5 +1,107 @@
 describe("Git content", function () {
   describe("when empty", function () {
+    describe.only("new tests", function () {
+      it("should initialize a Git repository", function () {
+        cy.resetData();
+        cy.visit("/git");
+        cy.fillSignInForm();
+
+        cy.findByText("Content directory is not tracked with Git").should(
+          "exist",
+        );
+
+        cy.findByText("Initialize").click();
+        cy.findByText("Content directory is not tracked with Git").should(
+          "not.exist",
+        );
+        cy.findByText("Branches").should("exist");
+      });
+
+      it("should create a new branch", function () {
+        cy.resetData();
+        cy.initializeContentGit();
+        cy.visit("/");
+        cy.fillSignInForm();
+
+        const newBranchName = "test-branch";
+
+        cy.findByText("Settings").click();
+        cy.findByText("Git").click();
+        cy.findByLabelText("Branch Name").type(newBranchName);
+        cy.findByText("Create").click();
+
+        cy.findByText(newBranchName).should("exist");
+      });
+      it("should checkout a branch", function () {
+        cy.resetData();
+        cy.initializeContentGit();
+        cy.visit("/");
+        cy.fillSignInForm();
+
+        const mainBranchName = "main";
+        const newBranchName = "test-branch";
+
+        cy.findByText("Settings").click();
+        cy.findByText("Git").click();
+        cy.findByText(newBranchName).click();
+        cy.findByText("Checkout").click();
+
+        cy.findByText(newBranchName).should("have.class", "font-bold");
+        cy.findByText(mainBranchName).should("not.have.class", "font-bold");
+      });
+      it("should delete a branch", function () {
+        cy.resetData();
+        cy.initializeContentGit();
+        cy.visit("/");
+        cy.fillSignInForm();
+
+        const newBranchName = "test-branch";
+
+        cy.findByText("Settings").click();
+        cy.findByText("Git").click();
+        cy.findByText(newBranchName).click();
+        cy.findByText("Delete").click();
+
+        cy.findByText(`The branch '${newBranchName}' has been deleted.`).should(
+          "exist",
+        );
+        cy.findByText(newBranchName).should("not.exist");
+      });
+      it("should force delete a branch", function () {
+        cy.resetData();
+        cy.initializeContentGit();
+        cy.visit("/");
+        cy.fillSignInForm();
+
+        const newBranchName = "test-branch";
+
+        cy.findByText("Settings").click();
+        cy.findByText("Git").click();
+        cy.findByLabelText("Branch Name").type(newBranchName);
+        cy.findByText("Create").click();
+
+        cy.findByText(newBranchName).click();
+        cy.findByText("Force Delete").click();
+
+        cy.findByText(`The branch '${newBranchName}' has been deleted.`).should(
+          "exist",
+        );
+        cy.findByText(newBranchName).should("not.exist");
+      });
+      it("should display error messages for invalid branch names", function () {
+        cy.resetData();
+        cy.initializeContentGit();
+        cy.visit("/");
+        cy.fillSignInForm();
+
+        cy.findByText("Settings").click();
+        cy.findByText("Git").click();
+        cy.findByText("Create").click();
+
+        cy.findByText("Branch Name is required").should("exist");
+      });
+    });
+
     it("should indicate when the content directory is not tracked by git", function () {
       cy.resetData();
       cy.visit("/git");
