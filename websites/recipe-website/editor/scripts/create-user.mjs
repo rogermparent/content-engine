a50643e6073a00f21377d169ca308fdb8ef59968
rogermparent <rogermparent@gmail.com>
2025-05-02 19:18:41 -0400

    Add docker

--- websites/recipe-website/editor/scripts/create-user.mjs
+++ websites/recipe-website/editor/scripts/create-user.mjs
@@ -3,17 +3,37 @@ import { mkdir, writeFile } from "fs/promises";
 import { resolve } from "path";
 import { read } from "read";
 import process from "node:process";
+import { parseArgs } from "node:util";
 
-async function createUser() {
+const options = {
+  email: {
+    type: "string",
+  },
+  password: {
+    type: "string",
+  },
+};
+
+async function getInput() {
+  // If the user provides an email and password via command line, use them
+  const { values } = parseArgs({ options });
+  if (values.email) {
+    return { email: values.email, password: values.password || "password" };
+  }
+  // Otherwise, prompt the user for an email and password
   const email = await read({ prompt: "Enter an email: " });
-  const passwordPromise = read({
+  const password = await read({
     prompt: "Enter a password: ",
     silent: true,
     replace: "*",
   });
+  return { email, password };
+}
 
+async function createUser() {
+  const inputPromise = getInput();
   const salt = await genSalt();
-  const password = await passwordPromise;
+  const { email, password } = await inputPromise;
   const hashedPassword = await hash(password, salt);
   const userData = { email, password: hashedPassword };
   const usersDir = resolve(process.env.CONTENT_DIRECTORY || "content", "users");
