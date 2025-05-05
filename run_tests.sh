6f514cfc9e5c2e7482c28bd9ba48d7ee218c0e08
rogermparent <rogermparent@gmail.com>
2025-05-05 12:00:20 -0400

    run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,9 @@
 apt-get update
 apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb git
 
+git config --global user.email "runner@example.com"
+git config --global user.name "Test Runner"
+
 # Install cypress
 pnpm exec cypress install
 
