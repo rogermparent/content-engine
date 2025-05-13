82573709701f14788e43cd1e7999e0d83f6e1075
Content Editor <content@example.com>
2025-05-12 20:35:24 -0400

    recipe form tests

---
+++ test/recipe-view.test.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { render, screen, within } from "@testing-library/react";
+
+import { test, expect } from "vitest";
+import { RecipeView } from "recipe-website-common/components/View";
+import userEvent from "@testing-library/user-event";
+
+const testRecipe = {
+  name: "Recipe 6",
+  description: "",
+  image: "recipe-6-test-image.png",
+  date: 1701998172622,
+  ingredients: [
+    { ingredient: '<Multiplyable baseNumber="1 1/2" /> tsp salt' },
+    { ingredient: '<Multiplyable baseNumber="1" /> cup water' },
+  ],
+  instructions: [
+    { text: 'Sprinkle <Multiplyable baseNumber="1/2" /> tsp salt in water' },
+    { text: "Boil water for a minute" },
+  ],
+};
+
+test("Can check off and reset ingredients", async () => {
+  render(await RecipeView({ recipe: testRecipe, slug: "recipe-6" }));
+
+  const saltRegex = /1 1\/2 tsp salt/;
+  const waterRegex = /1 cup water/;
+
+  expect(await screen.findByLabelText(saltRegex)).not.toBeChecked();
+
+  await userEvent.click(await screen.findByLabelText(saltRegex));
+
+  expect(await screen.findByLabelText(saltRegex)).toBeChecked();
+
+  expect(await screen.findByLabelText(waterRegex)).not.toBeChecked();
+
+  await userEvent.click(await screen.findByLabelText(waterRegex));
+
+  expect(await screen.findByLabelText(waterRegex)).toBeChecked();
+
+  await userEvent.click(
+    await within(
+      (await screen.findByText("Ingredients")).parentElement as HTMLElement,
+    ).findByText("Reset"),
+  );
+
+  expect(await screen.findByLabelText(saltRegex)).not.toBeChecked();
+  expect(await screen.findByLabelText(waterRegex)).not.toBeChecked();
+});
+
+test("Can check off and reset instructions", async () => {
+  render(await RecipeView({ recipe: testRecipe, slug: "recipe-6" }));
+
+  const saltRegex = /Sprinkle 1\/2 tsp salt in water/;
+  const waterRegex = /Boil water for a minute/;
+
+  expect(await screen.findByLabelText(saltRegex)).not.toBeChecked();
+
+  await userEvent.click(await screen.findByLabelText(saltRegex));
+
+  expect(await screen.findByLabelText(saltRegex)).toBeChecked();
+
+  expect(await screen.findByLabelText(waterRegex)).not.toBeChecked();
+
+  await userEvent.click(await screen.findByLabelText(waterRegex));
+
+  expect(await screen.findByLabelText(waterRegex)).toBeChecked();
+
+  await userEvent.click(
+    await within(
+      (await screen.findByText("Ingredients")).parentElement as HTMLElement,
+    ).findByText("Reset"),
+  );
+
+  expect(await screen.findByLabelText(saltRegex)).not.toBeChecked();
+  expect(await screen.findByLabelText(waterRegex)).not.toBeChecked();
+});
