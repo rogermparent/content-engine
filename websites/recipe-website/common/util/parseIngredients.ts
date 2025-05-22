bf3a3a7c7686a1b8a2f53997da5a8cdcce3421fb
Content Editor <content@example.com>
2025-05-21 21:14:38 -0400

    Ideal paren nonmultiplying

--- websites/recipe-website/common/util/parseIngredients.ts
+++ websites/recipe-website/common/util/parseIngredients.ts
@@ -1,5 +1,67 @@
 import type { Ingredient } from "../controller/types";
 
+function addMultipliersToNumbers(input: string) {
+  return input.replace(
+    /[0-9]+(?:\/[0-9]+|(?: and)? [0-9]+\/[0-9]+|\.[0-9]+)?(?![0-9]*(?:\.[0-9]+)? *%)/g,
+    (match) => {
+      const normalizedMatch = match.replace(" and", "");
+      return `<Multiplyable baseNumber="${normalizedMatch}" />`;
+    },
+  );
+}
+
+const perServingRegex = /[ 0-9\(](per|each|ea\.)([ \)]|$)/;
+
+function splitParentheses(input: string): string {
+  let start = 0;
+  let parenStart = 0;
+  let parenEnd = 0;
+  let level = 0;
+  let segments = [];
+  for (let i = 0; i < input.length; i++) {
+    const character = input[i];
+    if (character === "(") {
+      if (level === 0) {
+        parenStart = i + 1;
+      }
+      level += 1;
+    }
+    if (character === ")") {
+      if (level === 1) {
+        parenEnd = i;
+        const initialSegment = input.slice(start, parenStart);
+        const parenSegment = input.slice(parenStart, parenEnd);
+
+        if (initialSegment.match(perServingRegex)) {
+          return input;
+        }
+
+        start = parenEnd;
+        parenStart = parenEnd;
+        segments.push(initialSegment);
+        segments.push(parenSegment);
+      }
+      if (level > 0) {
+        level -= 1;
+      }
+    }
+  }
+
+  const finalSegment = input.slice(start);
+  if (finalSegment.match(perServingRegex)) {
+    return input;
+  }
+
+  segments.push(finalSegment);
+  return segments
+    .map((segment, i) => {
+      return i % 2 === 0
+        ? addMultipliersToNumbers(segment)
+        : splitParentheses(segment);
+    })
+    .join("");
+}
+
 export function createIngredient(inputLine: string): Ingredient | undefined {
   const trimmedInputLine = inputLine
     .replace(/^[ \t\r\n\f*\-•▪]*/, "")
@@ -15,13 +77,7 @@ export function createIngredient(inputLine: string): Ingredient | undefined {
   if (trimmedInputLine) {
     const isHeading = trimmedInputLine.endsWith(":");
 
-    const multiplyableIngredient = trimmedInputLine.replace(
-      /[0-9]+(?:\/[0-9]+|(?: and)? [0-9]+\/[0-9]+|\.[0-9]+)?(?![0-9]*(?:\.[0-9]+)? *%)/g,
-      (match) => {
-        const normalizedMatch = match.replace(" and", "");
-        return `<Multiplyable baseNumber="${normalizedMatch}" />`;
-      },
-    );
+    const multiplyableIngredient = splitParentheses(trimmedInputLine);
 
     return {
       ingredient: multiplyableIngredient,
