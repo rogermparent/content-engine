2290a01bc3497f8be74909632af526c5d9ef5b5d
Content Editor <content@example.com>
2025-05-30 19:33:57 -0400

    Visit tree page in tree test

--- websites/recipe-website/editor/cypress/e2e/tree.cy.ts
+++ websites/recipe-website/editor/cypress/e2e/tree.cy.ts
@@ -2,10 +2,10 @@ describe("Index Page", function () {
   describe("with linked recipes", function () {
     beforeEach(function () {
       cy.resetData("linked-recipes");
+      cy.visit("/tree");
     });
 
     it("should display a tree", function () {
-      cy.visit("/");
       cy.findByText("Baked Potatoes").should("exist");
     });
   });
