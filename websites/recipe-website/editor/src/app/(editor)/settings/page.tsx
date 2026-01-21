7db8a8b4c653476059476928071783dd66cd80d8
Roger <rogermparent@gmail.com>
2026-01-21 12:04:19 -0500

    Add featured recipe database reload button (#109)
    * Add a basic featured recipe DB reload button
    
    * Tweak margin/padding

--- websites/recipe-website/editor/src/app/(editor)/settings/page.tsx
+++ websites/recipe-website/editor/src/app/(editor)/settings/page.tsx
@@ -1,4 +1,5 @@
 import { rebuildRecipeIndex } from "recipe-editor/controller/actions";
+import { rebuildFeaturedRecipeIndex } from "recipe-editor/controller/actions/featuredRecipes";
 import { auth, signIn } from "@/auth";
 import { SubmitButton } from "component-library/components/SubmitButton";
 import {
@@ -18,9 +19,14 @@ export default async function SettingsPage() {
     <PageMain>
       <PageSection maxWidth="xl" grow>
         <PageHeading>Database</PageHeading>
-        <div className="p-2">
+        <div className="my-4 mx-2">
           <form action={rebuildRecipeIndex}>
-            <SubmitButton>Reload Database</SubmitButton>
+            <SubmitButton>Reload Recipe Database</SubmitButton>
+          </form>
+        </div>
+        <div className="my-4 mx-2">
+          <form action={rebuildFeaturedRecipeIndex}>
+            <SubmitButton>Reload Featured Recipe Database</SubmitButton>
           </form>
         </div>
       </PageSection>
