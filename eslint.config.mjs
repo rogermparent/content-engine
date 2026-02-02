021782ab868d31e35b55cfea960494c584db4ada
rogermparent <rogermparent@gmail.com>
2026-02-02 09:25:06 -0500

    fix eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,39 @@
-// @ts-check
-
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import pluginMocha from "eslint-plugin-mocha";
+import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  pluginMocha.configs.recommended,
   ...nextVitals,
+  ...nextTs,
+  {
+    settings: {
+      next: {
+        rootDir: [
+          "websites/portfolio/common",
+          "websites/portfolio/editor",
+          "websites/portfolio/export",
+          "websites/recipe-website/common",
+          "websites/recipe-website/editor",
+          "websites/recipe-website/export",
+          "websites/resume-builder",
+        ],
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
